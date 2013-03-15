117a5e94f167f7acdda8df162990d1cd407524bc
Kel Guerin <kguerin.optics@gmail.com>
2013-03-14 23:56:58 -0400

    Added barebones modulair manager class and qt integration

---
+++ include/modulair/modulair_manager.h
@@ -0,0 +1,16 @@
+#ifndef MODULAIR_MODULAIR_MANAGER_H
+#define MODULAIR_MODULAIR_MANAGER_H
+
+#include "QtGui"
+#include <QtGui/QApplication>
+#include <QObject>
+
+namespace modulair{
+  class ModulairManager{
+    public:
+      ModulairManager();
+      ~ModulairManager();
+  };
+}
+
+#endif
---
+++ src/modulair_manager.cpp
@@ -0,0 +1,7 @@
+#include <modulair/modulair_manager.h>
+
+namespace modulair{
+  ModulairManager::ModulairManager(){}
+  ModulairManager::~ModulairManager(){}
+}
+
